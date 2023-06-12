db93037385c5640d27b886d1035cdeb8ce954cb5
Justin Winstead <justinjwinstead@gmail.com>
2023-06-12 11:25:21 -0700

    Added POST request handling for Room creation in API

--- api/serializers.py
+++ api/serializers.py
@@ -13,3 +13,12 @@ class RoomSerializer(serializers.ModelSerializer):
             'votes_to_skip',
             'created_at'
         )
+
+# class to serialize CreateRoom POST request into Python   
+class CreateRoomSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Room
+        fields = (
+            'guest_can_pause',
+            'votes_to_skip'
+        )
